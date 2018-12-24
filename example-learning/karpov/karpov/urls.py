3f66da25a7c9b491de8ed1329d25266a3b218fa6
Mohammad Nasirifar <farnasirim@gmail.com>
2018-12-24 00:33:09 -0500

    Add form for add_hint

--- example-learning/karpov/karpov/urls.py
+++ example-learning/karpov/karpov/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import re_path, path, include
+
+from .base_views import index
 
 urlpatterns = [
+    path('questions/', include('mooc.urls')),
     path('admin/', admin.site.urls),
+    re_path('^.*$', index),
 ]
